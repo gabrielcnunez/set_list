f7b3181f1da66fc8c0f49d1dc5a2d79eea886e3d
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-10-13 10:50:29 -0700

    Add tests, methods for artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,19 @@
 class Artist < ApplicationRecord
   has_many :songs
 
+  # def songs
+  #   Song.where(artist_id: self.id)
+  # end
+
   def average_song_length
     self.songs.average(:length)
+  end
+
+  def song_count
+    self.songs.count
+  end
+
+  def wrote_song?(song)
+    songs.include?(song)
   end
 end
